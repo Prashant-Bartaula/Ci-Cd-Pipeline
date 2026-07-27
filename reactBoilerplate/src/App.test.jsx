4636523b6d3fc7a9637d48dfa59526c5b6bcb065
Prashant Bartaula <airmax50cent@gmail.com>
2026-07-27 20:13:42 +0545

    err: fix testing erros

--- reactBoilerplate/src/App.test.jsx
+++ reactBoilerplate/src/App.test.jsx
@@ -5,5 +5,5 @@ import App from "./App";
 test("renders heading", () => {
   render(<App />);
 
-  expect(screen.getByText("Hello CI/CD!")).toBeInTheDocument();
+  expect(screen.getByText("App")).toBeInTheDocument();
 });
